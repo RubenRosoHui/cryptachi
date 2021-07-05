2d7f5148da44a2bf853986019f843887c1180369
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-05 05:53:39 -0400

    Import Google fonts.

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -3,11 +3,18 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Encode+Sans+SC:wght@400;700&family=JetBrains+Mono:wght@400;700&display=swap');
+  /*
+      To use the fonts:
+      font-family: 'Encode Sans SC', sans-serif;
+      font-family: 'JetBrains Mono', monospace;
+  */
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
