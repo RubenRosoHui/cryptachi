a378230d20244ca9c332763193d370e46f3ca13a
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-05 06:17:45 -0400

    Minor edits.

--- packages/web/src/router/index.js
+++ packages/web/src/router/index.js
@@ -3,6 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes: []
-})
+});
 
-export default router
+export default router;
