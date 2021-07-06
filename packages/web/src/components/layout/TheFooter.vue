c68409d9036b13b724a08ed90569190f63952d95
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-06 02:42:51 -0400

    Finished the footer.

--- packages/web/src/components/layout/TheFooter.vue
+++ packages/web/src/components/layout/TheFooter.vue
@@ -1,5 +1,8 @@
 <template>
-	<h1>FOOTER</h1>
+	<div class="footer-container">
+		<p>Website by <a href="https://www.fosterhangdaan.com" target="_blank">Foster Hangdaan</a> and <a href="https://percy.tech" target="_blank">Matthew Percy</a>.</p>
+		<p class="google-terms font-sm white-dark">This site is protected by reCaptcha and the Google <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a> and <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.</p>
+	</div>
 </template>>
 
 <script>
@@ -9,4 +12,11 @@
 </script>
 
 <style scoped>
+	.footer-container {
+		text-align: center;
+	}
+
+	.google-terms {
+		margin-top: var(--spacing-4);
+	}
 </style>
