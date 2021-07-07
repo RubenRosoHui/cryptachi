6055e1f79090431401d44029727b5f3ec4feabe7
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-06 23:24:03 -0400

    Added and completed AdvantagesSection.vue.

--- packages/web/src/pages/home/Home.vue
+++ packages/web/src/pages/home/Home.vue
@@ -1,13 +1,16 @@
 <template>
 	<header-section />
 	<supported-cryptos-section />
+	<advantages-section />
 </template>
 
 <script>
 	import HeaderSection from './HeaderSection.vue';
 	import SupportedCryptosSection from './SupportedCryptosSection.vue';
+	import AdvantagesSection from './AdvantagesSection.vue';
+
 	export default {
-		components: { HeaderSection, SupportedCryptosSection },
+		components: { HeaderSection, SupportedCryptosSection, AdvantagesSection },
 		name: 'HomePage'
 	};
 </script>>
