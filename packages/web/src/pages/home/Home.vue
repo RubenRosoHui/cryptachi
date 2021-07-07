7458711fe31d7da1b3f268e13e01de3b32a1d779
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-06 23:53:24 -0400

    Added and completed SignupNowSection.vue.

--- packages/web/src/pages/home/Home.vue
+++ packages/web/src/pages/home/Home.vue
@@ -2,15 +2,17 @@
 	<header-section />
 	<supported-cryptos-section />
 	<advantages-section />
+	<signup-now-section />
 </template>
 
 <script>
 	import HeaderSection from './HeaderSection.vue';
 	import SupportedCryptosSection from './SupportedCryptosSection.vue';
 	import AdvantagesSection from './AdvantagesSection.vue';
+	import SignupNowSection from './SignupNowSection.vue';
 
 	export default {
-		components: { HeaderSection, SupportedCryptosSection, AdvantagesSection },
+		components: { HeaderSection, SupportedCryptosSection, AdvantagesSection, SignupNowSection },
 		name: 'HomePage'
 	};
 </script>>
