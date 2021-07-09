aa758a4936ea641a6b236afae1ac60bd03a25f46
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-09 07:50:44 -0400

    Added scrollBehavior options.

--- packages/web/src/router/index.js
+++ packages/web/src/router/index.js
@@ -5,6 +5,18 @@ import Register from '../pages/register/Register.vue';
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  scrollBehavior: (to, _from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      const scrollOptions = { behavior: 'smooth' };
+
+      if (to.hash) scrollOptions.el = to.hash;
+      else scrollOptions.top = 0;
+
+      return scrollOptions;
+    }
+  },
   routes: [
     { component: Home, path: '/' },
     { component: Register, path: '/register' }
