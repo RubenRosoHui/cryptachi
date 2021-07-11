8b4661d1a712e09868ff5717a5e1ec0e360549b8
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-10 23:19:32 -0400

    Removed 2FA status from header.
    The 2FA status is already included in the instructions.

--- packages/web/src/pages/account/AccountSecurity.vue
+++ packages/web/src/pages/account/AccountSecurity.vue
@@ -23,18 +23,18 @@
 		<div class="activate-2fa-container">
 			<header>
 				<h1>Activate 2FA</h1>
-				<span>Status: </span>
-				<span v-if="twoFactorAuthenticationEnabled" class="success">Enabled</span>
-				<span v-else class="error">Disabled</span>
 			</header>
 			<p class="margin-top-2 margin-bottom-2">
-				Two factor authentication is currently <span>disabled</span> on this account.
+				Two factor authentication is currently
+				<span v-if="twoFactorAuthenticationEnabled" class="success">enabled</span>
+				<span v-else class="error">disabled</span>
+				on this account.
 				To deactivate it, you must provide an OTP from your authenticator app.
 				You can use the the one-time paper key that was given to you when 2FA was activated.
 				If you lost your paper key, <router-link to="/contact">contact Cryptachi support</router-link>
 				and we will get the 2FA deactivation process started.
 			</p>
-			<p class="yellow bold">Upon successfully deactivating 2FA, all sessions for your user will be cleared.</p>
+			<p class="yellow bold">Upon successfully deactivating 2FA, all sessions for your user will be cleared requiring you to login again.</p>
 			<div class="buttons-container margin-top-4 text-align-right">
 				<button v-if="twoFactorAuthenticationEnabled" class="base-button">Deactivate</button>
 				<button v-else class="base-button">Activate</button>
