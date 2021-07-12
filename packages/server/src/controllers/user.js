c5593264518dbc3993f3245eb3f1fecefe6cc470
mrbru3 <matthew.david.percy@outlook.com>
2021-07-11 20:30:13 -0400

    changed name to alias

--- packages/server/src/controllers/user.js
+++ packages/server/src/controllers/user.js
@@ -2,7 +2,7 @@ const User = require('../models/user.js');
 const Alias = require('../models/alias.js');
 
 exports.getAliases = async (req, res, next) => {
-	const { name } = req.params;
+	const { alias } = req.params;
 	const user = await User.findById(req.user.id).populate("aliases");
 
 	return res.status(200).json(user.aliases);
