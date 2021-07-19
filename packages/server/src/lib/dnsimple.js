150ff6d6b5cad6b765b20c47ae058a7c3135111f
mrbru3 <matthew.david.percy@outlook.com>
2021-07-18 21:09:37 -0400

    Updated dnsimple api to us the domain from mongodb, with an optional override for testing purposes

--- packages/server/src/lib/dnsimple.js
+++ packages/server/src/lib/dnsimple.js
@@ -2,13 +2,15 @@ const fetch = require("node-fetch")
 const { sendEmail } = require("./email")
 
 //Create record in DNSimple and retrun the records ID
-exports.addRecord = async function(alias,domain,currency,address) {
+exports.addRecord = async function (alias, domain, currency, address) {
+	//Override domain if zone has been specified in .env. This is due to DNSimples sandbox not allowing the registration of existing domains
+	if (process.env.DNSIMPLE_ZONE) domain = process.env.DNSIMPLE_ZONE
 
 	//add zone record
 	let id;
-	await fetch(`${process.env.DNSIMPLE_DOMAIN}/${process.env.DNSIMPLE_ACCOUNTID}/zones/${process.env.DNSIMPLE_ZONE}/records`, {
+	await fetch(`${process.env.DNSIMPLE_DOMAIN}/${process.env.DNSIMPLE_ACCOUNTID}/zones/${domain}/records`, {
 		headers: {
-			'Authorization' : `Bearer ${process.env.DNSIMPLE_TOKEN}`,
+			'Authorization': `Bearer ${process.env.DNSIMPLE_TOKEN}`,
 			'Accept': 'application/json',
 			'Content-Type': 'application/json'
 		},
@@ -25,14 +27,17 @@ exports.addRecord = async function(alias,domain,currency,address) {
 	})
 	return id;
 }
-exports.deleteRecord = async function(id) {
-		await fetch(`${process.env.DNSIMPLE_DOMAIN}/${process.env.DNSIMPLE_ACCOUNTID}/zones/${process.env.DNSIMPLE_ZONE}/records/${id}`, {
-			headers: {
-				'Authorization' : `Bearer ${process.env.DNSIMPLE_TOKEN}`,
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			method: 'DELETE'
-	
-		})//.then(res => console.log(res))
+exports.deleteRecord = async function (id) {
+
+	if (process.env.DNSIMPLE_ZONE) domain = process.env.DNSIMPLE_ZONE
+
+	await fetch(`${process.env.DNSIMPLE_DOMAIN}/${process.env.DNSIMPLE_ACCOUNTID}/zones/${domain}/records/${id}`, {
+		headers: {
+			'Authorization': `Bearer ${process.env.DNSIMPLE_TOKEN}`,
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		method: 'DELETE'
+
+	})//.then(res => console.log(res))
 }
