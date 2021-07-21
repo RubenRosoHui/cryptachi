b2d973ccc1b35572ee5861f2c930969ac0937308
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-20 23:45:34 -0400

    Added ProgressBar component.

---
+++ packages/web/src/components/ui/ProgressBar.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="progress-bar">
+		<div class="progress" :style="progressStyle" />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'ProgressBar',
+		props: {
+			total: {
+				type: Number,
+				required: true
+			},
+			progress: {
+				type: Number,
+				required: true
+			}
+		},
+		computed: {
+			progressStyle() {
+				const progress = Math.floor(this.progress / this.total * 100);
+				return { width: progress.toString() + '%' };
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	div.progress-bar {
+		height: 30px;
+		border: 3px solid var(--cyan);
+	}
+
+	div.progress {
+		background-color: var(--cyan-dark);
+		height: 100%;
+		transition-property: width;
+		transition-duration: 0.3s;
+		transition-timing-function: linear;
+	}
+</style>
