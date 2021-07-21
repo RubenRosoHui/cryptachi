cdfbca8942aac9683c60f34161491c7622182bf5
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-21 05:16:40 -0400

    Updated FAQ and advantages section of home page.

--- packages/web/src/pages/faq/TheFAQ.vue
+++ packages/web/src/pages/faq/TheFAQ.vue
@@ -6,31 +6,51 @@
 		<ul class="faq-list">
 			<li>
 				<h2>What is Cryptachi?</h2>
-				<p>Placeholder</p>
+				<p>Cryptachi provides aliases (or names) for your crypto addresses. With an alias, you can give your sender a memorable name instead of a long and forgettable crypto address.</p>
+				<br />
+				<p>For the technically proficient, aliases are subdomains under our domain names. The payment information of aliases are stored as DNS TXT records using the <a href="https://openalias.org" target="_blank">OpenAlias</a> system. OpenAlias provides a standard for storing crypto payment information and is supported by several crypto wallets.</p>
 			</li>
 			<li>
 				<h2>Why Cryptachi?</h2>
-				<p>Placeholder</p>
+				<p>Typically you can create crypto aliases yourself by making subdomains within a domain you own. However, to obtain your own domain, you are legally required to provide accurate contact information to your registrar sacrificing your personal privacy.</p>
+				<br />
+				<p>At Cryptachi, all of our domains are registered under our information. As a result, we do not require your contact information when obtaining an alias letting you stay anonymous. We only require an email to sign up for a user account where it will be used for account management and payment related tasks such as sending password resets and receipts.</p>
+				<br />
+				<p>As an additional layer of security, all Cryptachi domains (both free and paid) are equipped with DNSSEC at no additional cost. DNSSEC is designed to protect transactions sent to your alias from being redirected to a malicious address owned by a hijacker.</p>
+				<br />
+				<p>With Cryptachi, you can obtain an affordable, secure and privacy-respecting alias in a few simple steps.</p>
 			</li>
 			<li>
 				<h2>What are records?</h2>
-				<p>Placeholder</p>
-			</li>
-			<li>
-				<h2>Are the aliases secure?</h2>
-				<p>Placeholder</p>
+				<p>A record is attached to an alias and stores the type of currency, recipient name, and recipient address. An alias may have multiple records where each record is for a specific currency but may only have one record per currency. <span class="yellow bold">Note</span> that free aliases are limited to 1 record and expire every 30 days.</p>
+				<br />
+				<p>You may edit these records at any time. Any changes to a record may take up to 24 hours to take effect.</p>
 			</li>
 			<li>
 				<h2>Which cryptocurrencies are supported?</h2>
-				<p>Placeholder</p>
+				<p>We currently support the following currencies with plans of adding many more:</p>
+				<ul class="base-list">
+					<li>Bitcoin</li>
+					<li>Ethereum</li>
+					<li>Monero</li>
+				</ul>
 			</li>
 			<li>
-				<h2>Which wallet supports OpenAlias?</h2>
-				<p>Placeholder</p>
+				<h2>Which wallets support OpenAlias?</h2>
+				<p>Crypto wallets with OpenAlias support are listed below. Refer to the <a target="_blank" href="https://openalias.org">OpenAlias</a> site for an up-to-date list.</p>
+				<ul class="base-list">
+					<li><a target="_blank" href="https://www.getmonero.org">Monero Core</a></li>
+					<li><a target="_blank" href="https://electrum.org">Electrum</a></li>
+					<li><a target="_blank" href="https://mymonero.com/">MyMonero</a></li>
+					<li><a target="_blank" href="https://coin.space/">Coin.Space</a></li>
+					<li><a target="_blank" href="https://github.com/nabijaczleweli/openalias.rs">openalias.rs</a></li>
+				</ul>
 			</li>
 			<li>
 				<h2>What type of payments are accepted?</h2>
-				<p>Placeholder</p>
+				<p>We accept crypto payments for the following currencies: Bitcoin, Monero, and Ethereum</p>
+				<br />
+				<p>We currently do not accept credit and debit card payments.</p>
 			</li>
 			<li>
 				<h2>What is your refund policy?</h2>
@@ -62,10 +82,10 @@
 		margin-bottom: var(--spacing-16);
 	}
 
-	ul.faq-list li {
+	ul.faq-list > li {
 		margin-top: var(--spacing-8);
 	}
-	ul.faq-list li:first-child {
+	ul.faq-list > li:first-child {
 		margin-top: 0;
 	}
 
--- packages/web/src/pages/home/AdvantagesSection.vue
+++ packages/web/src/pages/home/AdvantagesSection.vue
@@ -10,22 +10,30 @@
 					<img src="../../assets/icons/svg/fi-rr-eye-crossed.svg" />
 					<h2>Private</h2>
 				</header>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				<p>We accept crypto payments for several currencies including privacy-focused cryptos such as Monero.</p>
+			</li>
+			<li>
+				<header>
+					<img src="../../assets/icons/svg/fi-rr-incognito.svg" />
+					<h2>Anonymous</h2>
+				</header>
+				<p>We do not require or log your personal information when using our aliases. Only an email is needed for account registration and management.</p>
 			</li>
 			<li>
 				<header>
 					<img src="../../assets/icons/svg/fi-rr-shield-check.svg" />
 					<h2>Secure</h2>
 				</header>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>All Cryptachi domains (both free and paid) are equipped with DNSSEC. You can rest assured that your transactions are being sent to the proper addresses instead of maliciously redirected.</p>
 			</li>
 			<li>
 				<header>
 					<img src="../../assets/icons/svg/fi-rr-paper-plane.svg" />
 					<h2>Simple</h2>
 				</header>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Our interface is simple and easy-to-use. You can get started right away with a free alias without much hassle.</p>
 			</li>
+			<!--
 			<li>
 				<header>
 					<img src="../../assets/icons/svg/fi-rr-refresh.svg" />
@@ -33,19 +41,13 @@
 				</header>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</li>
+			-->
 			<li>
 				<header>
 					<img src="../../assets/icons/svg/fi-rr-infinity.svg" />
 					<h2>Reliable</h2>
 				</header>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</li>
-			<li>
-				<header>
-					<img src="../../assets/icons/svg/fi-rr-rec.svg" />
-					<h2>Placeholder</h2>
-				</header>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Aliases are stored in servers with redundancy and DDoS protection. Your alias will always be ready to receive transactions whenever you need it.</p>
 			</li>
 		</ul>
 	</div>
@@ -100,13 +102,15 @@
 			padding-left: var(--spacing-32);
 			padding-right: var(--spacing-32);
 		}
-		.advantages-list {
+		ul.advantages-list {
 			display: flex;
-			justify-content: space-evenly;
+			justify-content: space-between;
 			flex-wrap: wrap;
+			width: 950px;
+			margin: 0 auto;
 			gap: var(--spacing-8);
 		}
-		.advantages-list li {
+		ul.advantages-list li {
 			margin-top: 0;
 			max-width: 450px;
 		}
