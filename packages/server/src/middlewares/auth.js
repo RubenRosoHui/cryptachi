a97500e4b32df008137bc18c9320fda4f4df0295
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-25 14:27:52 -0400

    Further improvements to auth middleware.

--- packages/server/src/middlewares/auth.js
+++ packages/server/src/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require('../models/user.js');
 const errorLib = require('../lib/error.js');
 
-exports.needsWebToken = (req, res, next) => {
+exports.needsWebToken = async (req, res, next) => {
 	const token = req.header('Authorization');
 
 	if (!token) throw errorLib.authenticationError("Authentication token is required.");
@@ -10,6 +10,15 @@ exports.needsWebToken = (req, res, next) => {
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 		req.user = decoded;
+
+    // NOTE: This helps prevent many errors and redundancies where controllers/middlewares
+		// after this one assume that the decoded user exists. That is not always the case.
+    const user = await User.findById(req.user.id);
+		if (!user) throw errorLib.notFoundError('Authentication Failed: User not found.');
+
+    // Append additional info to the req.user object.
+		req.user.isEmailConfirmed = user.isEmailConfirmed;
+
 		next();
 	} catch (err) {
 		next(errorLib.errorWrapper(err));
@@ -19,11 +28,9 @@ exports.needsWebToken = (req, res, next) => {
 // NOTE: This needs the req.user object from the needsWebToken middleware.
 //       Always have the needsWebToken before this middleware.
 exports.needsVerifiedAccount = async (req, res, next) => {
-  // This is mostly to ensure that needsWebToken was properly added
+	// This is mostly to ensure that needsWebToken was properly added
 	// before this middleware and that it succeeded in validation.
-  if (!req.user) return next(errorLib.authenticationError('Authentication is required.'));
+  if (!req.user) return next(errorLib.authenticationError('Checking account confirmation failed: Authentication is required.'));
 
-  const user = await User.findById(req.user.id);
-
-  user.isEmailConfirmed ? next() : next(errorLib.unauthorizedAccessError('Access granted only to confirmed accounts.'));
+  req.user.isEmailConfirmed ? next() : next(errorLib.unauthorizedAccessError('Access granted only to confirmed accounts.'));
 }
