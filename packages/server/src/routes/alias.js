24965ae2473967f18a42e8dd7b1e2fb38b2b8ddb
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-07-27 02:32:23 -0400

    Removed validator.js
    Validator.js has been deprecated.

--- packages/server/src/routes/alias.js
+++ packages/server/src/routes/alias.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/alias.js')
 const authMiddleWare = require('../middlewares/auth.js')
-const validators = require('../validators/validators.js');
+// const validators = require('../validators/validators.js');
 
 //QUERY ALIAS
-router.get('/', validators.validateQueryAliases,controller.queryAliases);
+router.get('/', controller.queryAliases);
 
 module.exports = router;
