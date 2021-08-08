08722bdd8f39cfb7d55fa7a2d0027bc6ab22c6eb
mrbru3 <matthew.david.percy@outlook.com>
2021-08-07 23:23:18 -0400

    Added Validator to verify signatures

--- packages/server/src/routes/checkout.js
+++ packages/server/src/routes/checkout.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 
+const bodyParser = require('body-parser');
 const controller = require('../controllers/checkout.js');
 const validator = require('../validators/checkout.js');
 
 router.post('/create-invoice', controller.createInvoice)
 
-router.post('/webhooks/invoice-settled',controller.invoiceSettled)
-router.post('/webhooks/invoice-processing', controller.invoiceProcessing)
-router.post('/webhooks/invoice-expired',controller.invoiceExpired)
-router.post('/webhooks/invoice-invalid',controller.invoiceInvalid)
+router.post('/webhooks/test', validator.webhooks, controller.test)
 
 module.exports = router;
--- packages/server/src/validators/checkout.js
+++ packages/server/src/validators/checkout.js
@@ -1,4 +1,22 @@
 const validate = require('../lib/validate.js');
+const { check, body, param, query, oneOf,header, validationResult } = require('express-validator');
+const crypto = require('crypto')
+
+exports.webhooks = [
+	header('btcpay-sig').custom((signature, {req}) => {
+
+		const hmac = crypto.createHmac('sha256',process.env.WEBHOOK_SECRET)
+		.update(req.rawbody)
+		.digest('hex');
+
+		if(signature != `sha256=${hmac}`){
+			console.log('invalid webhook signature')
+			throw errorLib.authenticationError('You do not have permission to access this route');
+		}
+		return true;
+	})
+
+]
 
 exports.postPaymentIntent = [
   validate.alias({
