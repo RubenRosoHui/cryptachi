d068c4133d6d6c4456442fc274c1c7afb53f9027
mrbru3 <matthew.david.percy@outlook.com>
2021-08-09 00:10:04 -0400

    Query Alias route now omits aliases with an active invoice

--- packages/server/src/controllers/alias.js
+++ packages/server/src/controllers/alias.js
@@ -11,12 +11,15 @@ exports.queryAliases = async (req, res, next) => {
 		//find all currently taken domains
 		let aliases = names.split(',');
 
-		//TODO: aliases with an active invoice should not show up here
+		
 		const records = await Alias.find({ alias: aliases, domain: domain, user: { "$ne": null } }).select('alias -_id');
+		//REVIEW: aliases with an active invoice now do not show up here
+		const invoiceRecords = await Alias.find({ alias: aliases, domain: domain, invoice: { "$ne": null } }).select('alias -_id');
 
 		const takenAliases = records.map(r => r.alias);
+		const invoiceAliases = invoiceRecords.map(r => r.alias);
 
-		const availableAliases = aliases.filter(alias => !takenAliases.includes(alias));
+		const availableAliases = aliases.filter(alias => !takenAliases.includes(alias) && !invoiceAliases.includes(alias));
 
 		return res.status(200).json(availableAliases);
 	}
