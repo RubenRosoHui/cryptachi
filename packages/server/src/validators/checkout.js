b2d379f7947e870ef8cb9efbb0199fb1827604ec
mrbru3 <matthew.david.percy@outlook.com>
2021-08-09 00:14:43 -0400

    Removed old payment code

--- packages/server/src/validators/checkout.js
+++ packages/server/src/validators/checkout.js
@@ -39,17 +39,3 @@ exports.webhooks = [
 	}),
 	validate.checkValidationResults
 ]
-
-exports.postPaymentIntent = [
-	validate.alias({
-		checkValueIn: 'body',
-		checkDomainValueIn: 'body',
-		mustExist: true
-	}),
-	validate.domain({ checkValueIn: 'body', checkAliasValueIn: 'body' }),
-	validate.plan(),
-	validate.paymentCurrency(),
-	validate.paymentPrice(),
-	validate.paymentUnit(),
-	validate.checkValidationResults
-];
