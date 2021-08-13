d59f78f52d1b4777e6575ae4516bc1ebc2be9022
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-08-13 03:01:20 -0400

    Renamed Authenticator.vue
    Renamed to ManageAuth.vue and minor changes to instructions.

--- packages/web/src/pages/account/2fa/ManageAuth.vue
+++ packages/web/src/pages/account/2fa/ManageAuth.vue
@@ -1,6 +1,6 @@
 <template>
 	<header>
-		<h1>Activate 2FA</h1>
+		<h1>Manage 2FA</h1>
 	</header>
 	<div v-if="!isLoading">
 		<p class="margin-top-2 margin-bottom-2">
@@ -8,15 +8,15 @@
 			<span v-if="twoFactorAuthenticationEnabled" class="success">enabled</span>
 			<span v-else class="error">disabled</span>
 			on this account.
-			To deactivate it, you must provide an OTP from your authenticator app.
+			To disable it, you must provide an OTP from your authenticator app.
 			You can use the the one-time paper key that was given to you when 2FA was activated.
 			If you lost your paper key, <router-link to="/contact">contact Cryptachi support</router-link>
 			and we will get the 2FA deactivation process started.
 		</p>
-		<p class="yellow bold">Upon successfully deactivating 2FA, all sessions for your user will be cleared requiring you to login again.</p>
+		<p class="yellow bold">Upon successfully disabling 2FA, all sessions for your user will be cleared requiring you to login again.</p>
 		<div class="buttons-container margin-top-4 text-align-right">
-			<button v-if="twoFactorAuthenticationEnabled" class="base-button" @click="$emit('changeAuthComponent', 'DisableAuth')">Deactivate</button>
-			<button v-else class="base-button" @click="$emit('changeAuthComponent', 'EnableAuth')">Activate</button>
+			<button v-if="twoFactorAuthenticationEnabled" class="base-button deactivate" @click="$emit('changeAuthComponent', 'DisableAuth')">Disable</button>
+			<button v-else class="base-button" @click="$emit('changeAuthComponent', 'EnableAuth')">Enable</button>
 		</div>
 	</div>
 	<div class="text-align-center" v-else>
@@ -27,7 +27,7 @@
 
 <script>
 	export default {
-		name: 'Authenticator',
+		name: 'ManageAuth',
 		emits: ['changeAuthComponent'],
 		data: () => ({
 			twoFactorAuthenticationEnabled: false,
@@ -55,4 +55,7 @@
 </script>
 
 <style scoped>
+	button.deactivate {
+		background-color: var(--red);
+	}
 </style>
