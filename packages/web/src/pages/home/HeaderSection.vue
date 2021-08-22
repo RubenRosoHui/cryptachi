bc18d1e7413ca91de9e5671f86629b44c9c5e299
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-08-22 16:51:33 -0400

    Adjust logo animation delay.

--- packages/web/src/pages/home/HeaderSection.vue
+++ packages/web/src/pages/home/HeaderSection.vue
@@ -80,10 +80,12 @@
 	}
 	.logo-left {
 		animation: slide-from-left 2s forwards;
+		animation-delay: -1s;
 		animation-timing-function: ease-out;
 	}
 	.logo-right {
 		animation: slide-from-right 2s forwards;
+		animation-delay: -1s;
 		animation-timing-function: ease-out;
 	}
 	@keyframes slide-from-left {
